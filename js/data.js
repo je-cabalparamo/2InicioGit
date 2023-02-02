f820a92cfcd6c03875f1ef84511c5f57126fb0ce
unknown <je.cabalparamo@ugto.mx>
2023-02-02 12:12:29 -0600

    Implementacion de JS :)

---
+++ js/data.js
@@ -0,0 +1,92 @@
+const songs = [
+    {
+        name:'Diablo Rojo',
+        path: '../Music/Rodrigo y Gabriela - Diablo Rojo.mp3',
+        artist: 'Rodrigo y Gabriela',
+        cover: '../Images/diablo rojo.jpg'
+    },
+    {
+        name:'Master Exploder',
+        path: '../Music/07 - Master Exploder.mp3',
+        artist: 'Tenacious D',
+        cover: '../Images/TenaciousDInThePickOfDestinySoundtrack.jpg'
+    },
+    {
+        name:'Voodoo Mama',
+        path: '../Music/Voodoo Mama (Music from the Motion Picture Babylon).mp3',
+        artist: 'Justin Hurwitz',
+        cover: '../Images/Babylon.jpg'
+    },
+    {
+        name:'Dance!',
+        path: '../Music/01 - Dance!.mp3',
+        artist: 'Shihoko Hirata',
+        cover: '../Images/P4Dance.png'
+    },
+    {
+        name:'Without Question',
+        path: '../Music/01 - Without Question (Movie Version).mp3',
+        artist: 'Elton John',
+        cover: '../Images/diablo rojo.jpg'
+    },
+    {
+        name:'You Rock My World',
+        path: '../Music/17 You Rock My World.mp3',
+        artist: 'Michael Jackson',
+        cover: '../Images/Yrmwsingle.png'
+    },
+    {
+        name:'Circles',
+        path: '../Music/Circles.mp3',
+        artist: 'Post Malone',
+        cover: '../Images/Hollywoods_Bleeding.png'
+    },
+    {
+        name:'Don',
+        path: '../Music/Don.mp3',
+        artist: 'Miranda',
+        cover: '../Images/Don.jpg'
+    },
+    {
+        name:'Don´t Stop Me Now',
+        path: '../Music/Dont Stop Me Now.mp3',
+        artist: 'Queen',
+        cover: '../Images/Queen_Jazz.png'
+    },
+    {
+        name:'EARFQUAKE',
+        path: '../Music/EARFQUAKE.mp3',
+        artist: 'Tyler The Creator',
+        cover: '../Images/Igor.png'
+    },
+    {
+        name:'ELEMENT.',
+        path: '../Music/ELEMENT..mp3',
+        artist: 'Kendrick Lamar',
+        cover: '../Images/Damn.png'
+    },
+    {
+        name:'Enemy',
+        path: '../Music/Enemy (from the series Arcane League of Legends).mp3',
+        artist: 'Imagine Dragons',
+        cover: '../Images/Enemy.png'
+    },
+    {
+        name:'Everybody Wants to Rule The World',
+        path: '../Music/Everybody Wants To Rule The World.mp3',
+        artist: 'Tears for Fears',
+        cover: '../Images/everybodywants.png'
+    },
+    {
+        name:'Glimpse of Us',
+        path: '../Music/Glimpse of Us.mp3',
+        artist: 'Joji',
+        cover: '../Images/Joji_-_Glimpse_of_Us.png'
+    },
+    {
+        name:'La Camisa Negra',
+        path: '../Music/La Camisa Negra.mp3',
+        artist: 'Juanes',
+        cover: '../Images/camisanegra.png'
+    },
+]
